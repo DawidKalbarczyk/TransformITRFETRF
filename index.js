b9c1a91640a2974fe34b6e4367dc714436b67d5f
Dawid <u56128@student.wat.edu.pl>
2026-06-14 12:41:19 +0200

    del: deleted body background changing photos

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const first_step_div = document.getElementById('content-div');
 const second_step_div = document.getElementById('second-step-div');
 
 document.getElementById('itrf-to-etrf-button').addEventListener('click', function() {
-    document.body.style.backgroundImage = "url('images_resized/milky-way-galaxy-dark-night-sky.jpg')";
     ITRF_fieldset.style.display = 'flex';
     ITRF_fieldset.style.opacity = '1';
     ETRF_fieldset.style.display = 'none';
@@ -20,7 +19,6 @@ document.getElementById('itrf-to-etrf-button').addEventListener('click', functio
 });
 
 document.getElementById('etrf-to-itrf-button').addEventListener('click', function() {
-    document.body.style.backgroundImage = "url('images_resized/milky-way-galaxy-dark-night-sky.jpg')";
     ETRF_fieldset.style.display = 'flex';
     ETRF_fieldset.style.opacity = '1';
     ITRF_fieldset.style.display = 'none';
@@ -28,6 +26,9 @@ document.getElementById('etrf-to-itrf-button').addEventListener('click', functio
     renderDivs(document.getElementById('fieldset_ETRF_2'), names_ETRF, 'legend-from');
     renderDivs(document.getElementById('fieldset_ITRF_2'), names_ITRF, 'legend-to');
     second_step_div.style.opacity = '1';
+    if (!first_step_div.classList.contains('first-step-div-transformed')) {
+        first_step_div.classList.add('first-step-div-transformed');
+    }
 });
 
 
